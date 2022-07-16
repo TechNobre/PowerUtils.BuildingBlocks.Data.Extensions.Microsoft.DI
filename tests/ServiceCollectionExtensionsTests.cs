a192a075b83436c7662614d4ff562e34de5665d5
Nelson Nobre <technobre@gmail.com>
2022-07-16 09:57:15 +0100

    test: apply pattern Given When Then

--- tests/ServiceCollectionExtensionsTests.cs
+++ tests/ServiceCollectionExtensionsTests.cs
@@ -7,7 +7,6 @@ using PowerUtils.xUnit.Extensions;
 
 namespace PowerUtils.BuildingBlocks.Data.Extensions.Microsoft.DI.Tests;
 
-[Trait("Extension", "Extensions")]
 public class ServiceCollectionExtensionsTests
 {
     private readonly IServiceCollection _services;
@@ -15,8 +14,10 @@ public class ServiceCollectionExtensionsTests
     public ServiceCollectionExtensionsTests()
         => _services = new ServiceCollection();
 
-    [Fact(DisplayName = "AddRepositories without passing the Assembly - The container should have more zero services")]
-    public void AddRepositories_WithoutAssembly_GreaterThanZero()
+
+
+    [Fact]
+    public void WithoutAssembly_AddRepositories_GreaterThanZero()
     {
         // Arrange & Act
         _services.AddRepositories();
@@ -27,8 +28,8 @@ public class ServiceCollectionExtensionsTests
             .HaveCountGreaterThan(0);
     }
 
-    [Fact(DisplayName = "AddRepositories with passing the Assembly - The container should have more zero services")]
-    public void AddRepositories_WithAssembly_GreaterThanZero()
+    [Fact]
+    public void WithAssembly_AddRepositories_GreaterThanZero()
     {
         // Arrange & Act
         _services.AddRepositories(typeof(ServiceCollectionExtensionsTests).Assembly);
@@ -39,8 +40,8 @@ public class ServiceCollectionExtensionsTests
             .HaveCountGreaterThan(0);
     }
 
-    [Fact(DisplayName = "Try add repositories from an assembly without repositories - Should not add repositories")]
-    public void AddRepositories_AssemblyWithoutRepositories_ZeroRepositories()
+    [Fact]
+    public void AssemblyWithoutRepositories_AddRepositories_ZeroRepositories()
     {
         // Arrange & Act
         _services.AddRepositories(ServiceLifetime.Singleton, typeof(IRepositoryBase).Assembly);
@@ -51,8 +52,8 @@ public class ServiceCollectionExtensionsTests
             .HaveCount(0);
     }
 
-    [Fact(DisplayName = "AddRepositories with life time transient - Should add the repository")]
-    public void ServiceLifetime_Transient_Added()
+    [Fact]
+    public void ServiceLifetimeTransient_AddRepositories_GreaterThanZero()
     {
         // Arrange & Act
         _services.AddRepositories(ServiceLifetime.Transient, typeof(ServiceCollectionExtensionsTests).Assembly);
@@ -63,8 +64,8 @@ public class ServiceCollectionExtensionsTests
             .HaveCountGreaterThan(0);
     }
 
-    [Fact(DisplayName = "AddRepositories with life time scoped - Should add the repository")]
-    public void ServiceLifetime_Scoped_Added()
+    [Fact]
+    public void ServiceLifetimeScoped_AddRepositories_GreaterThanZero()
     {
         // Arrange & Act
         _services.AddRepositories(ServiceLifetime.Scoped, typeof(ServiceCollectionExtensionsTests).Assembly);
@@ -75,8 +76,8 @@ public class ServiceCollectionExtensionsTests
             .HaveCountGreaterThan(0);
     }
 
-    [Fact(DisplayName = "AddRepositories with life time singleton - Should add the repository")]
-    public void ServiceLifetime_Singleton_Added()
+    [Fact]
+    public void ServiceLifetimeSingleton_AddRepositories_GreaterThanZero()
     {
         // Arrange & Act
         _services.AddRepositories(ServiceLifetime.Singleton, typeof(ServiceCollectionExtensionsTests).Assembly);
@@ -87,8 +88,8 @@ public class ServiceCollectionExtensionsTests
             .HaveCountGreaterThan(0);
     }
 
-    [Fact(DisplayName = "Try add repositories with null assembly - Should return exception")]
-    public void AddRepositories_NullAssemblies_Exception()
+    [Fact]
+    public void NullAssemblies_AddRepositories_Exception()
     {
         // Arrange & Act
         var act = Record.Exception(() => _services.AddRepositories(ServiceLifetime.Singleton, null));
@@ -99,8 +100,8 @@ public class ServiceCollectionExtensionsTests
             .BeOfType<ArgumentException>();
     }
 
-    [Fact(DisplayName = "Try add repositories with empty assembly - Should return exception")]
-    public void AddRepositories_EmptyAssemblies_Exception()
+    [Fact]
+    public void EmptyAssemblies_AddRepositories_Exception()
     {
         // Arrange
         var assembly = Array.Empty<Assembly>();
@@ -115,8 +116,8 @@ public class ServiceCollectionExtensionsTests
             .BeOfType<ArgumentException>();
     }
 
-    [Fact(DisplayName = "Try add repositories twices - Should return exception")]
-    public void RegisterService_Twices_Exception()
+    [Fact]
+    public void TwicesSameInterface_RegisterService_Exception()
     {
         // Arrange
         _services.AddRepositories();
